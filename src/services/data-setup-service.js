89619cb8f8ec28dd54162a5d18275efa7a9d938c
Michael Sullivan <msullivan@myriadmobile.com>
2018-11-01 16:37:47 -0500

    WIP removing duplicates

--- src/services/data-setup-service.js
+++ src/services/data-setup-service.js
@@ -145,7 +145,6 @@ class DataSetupService {
 
 
         //List of all correlated object data
-
         const correlatedData = [];
 
         //Add a counter to enable keys in list
@@ -153,67 +152,84 @@ class DataSetupService {
 
         allListsKeys.forEach(key => {
 
+            console.log(correlatedData);
+
             const list = allLists[key];
 
-            Object.keys(allLists).forEach(doKey => {
-
+            Object.keys(allLists).forEach(allListsKey => {
                 //TODO: This will error out if there are not enough data points
 
-                const correlation1 = getPearsonCorrelation(list, allLists[doKey]);
-                const correlationDirection1 = correlation1 > 0 ? 'positive' : 'negative';
+                //Check to see if an inverted version of this row already exists
+                const isAReverseDuplicate = correlatedData.find(row => {
+                    if (row.field ===allListsKey && row.compared_against === key) {
+                       return true;
+                    }
+                });
 
-                const correlation2 = getPearsonCorrelation(list.slice(1), allLists[doKey].slice(0,-1));
-                const correlationDirection2 = correlation2 > 0 ? 'positive' : 'negative';
+                //If it does, or if the record is equal to the current key skip it completely
+                if (key !== allListsKey && !isAReverseDuplicate) {
 
-                const correlation3 = getPearsonCorrelation(list.slice(2), allLists[doKey].slice(0,-2));
-                const correlationDirection3 = correlation3 > 0 ? 'positive' : 'negative';
+                    const correlation1 = getPearsonCorrelation(list, allLists[allListsKey]);
+                    const correlationDirection1 = correlation1 > 0 ? 'positive' : 'negative';
 
-                const correlation4 = getPearsonCorrelation(list.slice(3), allLists[doKey].slice(0,-3));
-                const correlationDirection4 = correlation4 > 0 ? 'positive' : 'negative';
+                    const correlation2 = getPearsonCorrelation(list.slice(1), allLists[allListsKey].slice(0,-1));
+                    const correlationDirection2 = correlation2 > 0 ? 'positive' : 'negative';
 
-                const correlation5 = getPearsonCorrelation(list.slice(4), allLists[doKey].slice(0,-4));
-                const correlationDirection5 = correlation5 > 0 ? 'positive' : 'negative';
+                    const correlation3 = getPearsonCorrelation(list.slice(2), allLists[allListsKey].slice(0,-2));
+                    const correlationDirection3 = correlation3 > 0 ? 'positive' : 'negative';
 
-                const correlation_total = correlation1 + correlation2 + correlation3 + correlation4 + correlation5;
+                    const correlation4 = getPearsonCorrelation(list.slice(3), allLists[allListsKey].slice(0,-3));
+                    const correlationDirection4 = correlation4 > 0 ? 'positive' : 'negative';
 
-                counter++;
+                    const correlation5 = getPearsonCorrelation(list.slice(4), allLists[allListsKey].slice(0,-4));
+                    const correlationDirection5 = correlation5 > 0 ? 'positive' : 'negative';
 
-                correlatedData.push({
-                    key: counter,
-                    field : key,
-                    compared_against : doKey,
-                    correlation_total: Math.abs(correlation_total),
-                    correlation_avg: Math.abs(correlation_total/5),
-                    correlation_total_direction: correlation_total > 0 ? 'positive' : 'negative',
-                    correlations: [
-                        {
-                            day: 1,
-                            correlation : Math.abs(correlation1),
-                            correlation_direction : correlationDirection1,
-                        },
-                        {
-                            day: 2,
-                            correlation : Math.abs(correlation2),
-                            correlation_direction : correlationDirection2,
-                        },
-                        {
-                            day: 3,
-                            correlation : Math.abs(correlation3),
-                            correlation_direction : correlationDirection3,
-                        },
-                        {
-                            day: 4,
-                            correlation : Math.abs(correlation4),
-                            correlation_direction : correlationDirection4,
-                        },
-                        {
-                            day: 5,
-                            correlation : Math.abs(correlation5),
-                            correlation_direction : correlationDirection5,
-                        },
-                    ]
+                    const correlation_total = correlation1 + correlation2 + correlation3 + correlation4 + correlation5;
 
-                })
+                    counter++;
+
+                    correlatedData.push({
+                        key: counter,
+                        field : key,
+                        compared_against : allListsKey,
+                        correlation_total: Math.abs(correlation_total),
+                        correlation_avg: Math.abs(correlation_total/5),
+                        correlation_total_direction: correlation_total > 0 ? 'positive' : 'negative',
+                        correlations: [
+                            {
+                                day: 1,
+                                correlation : Math.abs(correlation1),
+                                correlation_direction : correlationDirection1,
+                            },
+                            {
+                                day: 2,
+                                correlation : Math.abs(correlation2),
+                                correlation_direction : correlationDirection2,
+                            },
+                            {
+                                day: 3,
+                                correlation : Math.abs(correlation3),
+                                correlation_direction : correlationDirection3,
+                            },
+                            {
+                                day: 4,
+                                correlation : Math.abs(correlation4),
+                                correlation_direction : correlationDirection4,
+                            },
+                            {
+                                day: 5,
+                                correlation : Math.abs(correlation5),
+                                correlation_direction : correlationDirection5,
+                            },
+                        ]
+
+                    })
+
+                }
+
+                else {
+                    console.log('Is duplicate');
+                }
 
             })
 
